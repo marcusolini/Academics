5abb0ce698856d5542e8991a1dbc1ec5379d8fb4
Mark Nicolini <marcusolini@outlook.com>
2018-03-26 21:19:50 -0400

    Consumer Producer Threading Project

---
+++ ProducerConsumer/Consumer.h
@@ -0,0 +1,57 @@
+// Copyright(c) 2018 Mark Nicolini
+
+#pragma once
+
+#include "ProdConQueue.h"
+
+template <class DATA> class CConsumer
+{
+public:
+     CConsumer(CProdConQueue<DATA>& queue, size_t quantity, const size_t removeDelay = 0);
+     virtual ~CConsumer();
+     DATA& Remove();
+     size_t Quantity();
+
+     CConsumer(const CConsumer&) = delete;
+     CConsumer& operator=(const CConsumer&) = delete;
+
+private:
+     CProdConQueue<DATA>& m_queue;
+     size_t m_quantity = 0;
+     size_t m_removeDelay = 0;  // milliseconds
+};
+
+
+template <class DATA>
+CConsumer<DATA>::CConsumer(CProdConQueue<DATA>& queue, const size_t quantity, const size_t removeDelay)
+     : m_queue(queue), m_quantity(quantity), m_removeDelay(removeDelay)
+{
+     std::wcout << __FUNCTION__ << ":" << __LINE__ << std::endl;
+}
+
+template <class DATA>
+CConsumer<DATA>::~CConsumer()
+{
+     std::wcout << __FUNCTION__ << ":" << __LINE__ << std::endl;
+}
+
+template <class DATA>
+DATA& CConsumer<DATA>::Remove()
+{
+     std::wcout << __FUNCTION__ << ":" << __LINE__ << std::endl;
+
+     if (m_removeDelay)
+     {
+          Sleep(m_removeDelay);
+     }
+
+     return m_queue.Remove();
+}
+
+template <class DATA>
+size_t CConsumer<DATA>::Quantity()
+{
+     std::wcout << __FUNCTION__ << ":" << __LINE__ << std::endl;
+
+     return m_quantity;
+}
---
+++ ProducerConsumer/Producer.h
@@ -0,0 +1,76 @@
+// Copyright(c) 2018 Mark Nicolini
+
+#pragma once
+
+#include "ProdConQueue.h"
+
+template <class DATA> class CProducer
+{
+public:
+     CProducer(CProdConQueue<DATA>& queue, const size_t quantity, const size_t createDelay = 0, const size_t addDelay = 0);
+     virtual ~CProducer();
+     DATA& Create();
+     void Add(DATA& data);
+     size_t Quantity();
+
+     CProducer(const CProducer&) = delete;
+     CProducer& operator=(const CProducer&) = delete;
+
+private:
+     CProdConQueue<DATA>& m_queue;
+     size_t m_quantity = 0;
+     size_t m_createDelay = 0;  // milliseconds
+     size_t m_addDelay = 0;  // milliseconds
+     DATA m_data = 0;
+};
+
+
+template <class DATA>
+CProducer<DATA>::CProducer(CProdConQueue<DATA>& queue, const size_t quantity, const size_t createDelay, const size_t addDelay)
+     : m_queue(queue), m_quantity(quantity), m_createDelay(createDelay), m_addDelay(addDelay)
+{
+     std::wcout << __FUNCTION__ << ":" << __LINE__ << std::endl;
+}
+
+template <class DATA>
+CProducer<DATA>::~CProducer()
+{
+     std::wcout << __FUNCTION__ << ":" << __LINE__ << std::endl;
+}
+
+template <class DATA>
+DATA& CProducer<DATA>::Create()
+{
+     std::wcout << __FUNCTION__ << ":" << __LINE__ << std::endl;
+
+     if (m_createDelay)
+     {
+          Sleep(m_createDelay);
+     }
+
+     m_data += 1;
+
+     return m_data;
+}
+
+
+template <class DATA>
+void CProducer<DATA>::Add(DATA& data)
+{
+     std::wcout << __FUNCTION__ << ":" << __LINE__ << std::endl;
+
+     if (m_addDelay)
+     {
+          Sleep(m_addDelay);
+     }
+
+     m_queue.Add(data);
+}
+
+template <class DATA>
+size_t CProducer<DATA>::Quantity()
+{
+     std::wcout << __FUNCTION__ << ":" << __LINE__ << std::endl;
+
+     return m_quantity;
+}
