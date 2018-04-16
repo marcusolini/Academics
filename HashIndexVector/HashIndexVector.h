4288158157464423f08c8fdc7bba9919fc644552
Mark Nicolini <marcusolini@outlook.com>
2018-04-15 20:59:43 -0400

    Added Apache License and Copyright

--- HashIndexVector/HashIndexVector.h
+++ HashIndexVector/HashIndexVector.h
@@ -1,4 +1,7 @@
-// Copyright(c) 2018 marcusolini@outlook.com
+// Copyright 2018 marcusolini@outlook.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 
 #pragma once
 
