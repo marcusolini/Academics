706b04d4cce24f9de709df4fbfc97637a096972d
Mark Nicolini <marcusolini@outlook.com>
2018-05-12 17:12:59 -0400

    RESTful Api: Added Powershell Github examples

---
+++ Web/REST-API/Powershell-Git-Examples/Github-Restful-Api-Client-Eamples.ps1
@@ -0,0 +1,75 @@
+﻿## REST Powershell
+## Github RESTful API Examples
+## Create and use a personal access token: https://github.com/settings/tokens
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$username = "Enter-name-of-authenticated-user"
+$token = "Enter-authenticated-user-token-or-password"
+$userToken = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}" -f $token)))
+$userrepo = "Enter-repository-user-name"
+$reponame = "Enter-existing-repository-name"
+
+$createRepo = "Enter-repository-name-to-be-created"
+$postRepoBody = @{name=$createRepo}
+$postRepoJson = $postRepoBody | ConvertTo-Json
+
+try
+{
+    $response = Invoke-RestMethod https://api.github.com -Method Get -Headers $auth
+    Write-Output $response
+}
+catch
+{
+    Write-Error "Failure '$_.Exception.Response.StatusDescription' connecting to Restful Github" -ErrorId $_.Exception.Response.StatusCode.value__
+}
+
+try
+{
+    $response = Invoke-RestMethod https://api.github.com/user/repos -Method Get -Headers $auth
+    Write-Output $response
+}
+catch
+{
+    Write-Error "Failure '$_.Exception.Response.StatusDescription' getting authenticated user '$username' Github repositories" -ErrorId $_.Exception.Response.StatusCode.value__
+}
+
+try
+{
+    $response = Invoke-RestMethod https://api.github.com/$userrepo/$reponame -Method Get -Headers $auth
+    Write-Output $response
+}
+catch
+{
+    Write-Error "Failure '$_.Exception.Response.StatusDescription' getting user '$userrepo' repository '$reponame'" -ErrorId $_.Exception.Response.StatusCode.value__
+}
+
+try
+{
+    $response = Invoke-RestMethod https://api.github.com/$userrepo/$reponame/subscription -Method Get -Headers $auth
+    Write-Output $response
+}
+catch
+{
+    Write-Error "Failure '$_.Exception.Response.StatusDescription' getting user '$userrepo' repository '$reponame' subscriptions" -ErrorId $_.Exception.Response.StatusCode.value__
+}
+
+try
+{
+    $response = Invoke-RestMethod https://api.github.com/$userrepo/$reponame/subscribers -Method Get -Headers $auth
+    Write-Output $response
+}
+catch
+{
+    Write-Error "Failure '$_.Exception.Response.StatusDescription' getting user '$userrepo' repository '$reponame' subscribers" -ErrorId $_.Exception.Response.StatusCode.value__
+}
+
+try
+{
+    $response = Invoke-RestMethod https://api.github.com/user/repos -Method Post -Headers @{Authorization = "Basic $userToken"} -Body $postRepoJson -ContentType 'application/json'
+    Write-Output $response
+}
+catch
+{
+    Write-Error "Failure '$_.Exception.Response.StatusDescription' creating authenticated user '$username' Github repository '$createRepo'" -ErrorId $_.Exception.Response.StatusCode.value__
+}
