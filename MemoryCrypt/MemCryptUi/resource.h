239e8d401e47b610734c4569ca76a431acf31502
Mark Nicolini <marcusolini@outlook.com>
2018-05-18 23:11:11 -0400

    Added Memory Encryption Functions with Console and Ui

---
+++ MemoryCrypt/MemCryptUi/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MemCryptUi.rc
+//
+#define IDS_RESOURCE_STRING_MISSING     104
+#define IDS_ENCRYPT                     105
+#define IDS_DECRYPT                     106
+#define IDS_CLOSE                       108
+#define IDS_CLOSE_QUESTION              109
+#define IDS_MEMORY_ENCRYPTION_DIALOG    110
+#define IDS_INVALID_DATA                111
+#define IDS_ENTER_VALID_DATA            112
+#define IDS_ENCRYPTION_FAILURE          113
+#define IDS_UNABLE_TO_ENCRYPT           114
+#define IDS_UNABLE_TO_DECRYPT           115
+#define IDC_DataEdit                    1001
+#define IDC_EncryptBtn                  1002
+#define IDD_MemEncryptDialog            10001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        10002
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
